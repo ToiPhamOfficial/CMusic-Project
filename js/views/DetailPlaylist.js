88901af530277de394e476ce24240666bb3dfbbd
toiphamofficial <toiphamofficial@gmail.com>
2026-01-15 15:59:10 +0700

    - Thêm responsive dropdown cho Player expaned - Đổi tên Playlist.js thành Playlists.js để phù hợp với chức năng hiển thị nhiều playlist - Thêm dữ liệu mẫu vào data.js cho các playlist - Cập nhật Explore.js để hiển thị các playlist Lofi từ data.js - Cập nhật các liên kết và tham số URL để phù hợp với thay đổi mới của playlist

--- js/views/DetailPlaylist.js
+++ js/views/DetailPlaylist.js
@@ -2,28 +2,19 @@ import { getPlaylistById, getSongById } from '../data.js';
 import { SongItem, HeaderSongItem } from '../components/Card.js';
 
 export default function PlaylistDetail() {
-    //Tự lấy ID từ URL hiện tại (ví dụ: .../artist-detail?id=1)
+    //Tự lấy ID từ URL hiện tại (ví dụ: .../playlist-detail?id=1)
     const params = new URLSearchParams(window.location.search);
-    const id = parseInt(params.get('id')); // Lấy số 1 ra
+    const id = parseInt(params.get('id'));
+    const type = params.get('type');
 
-    const playlist = getPlaylistById(id);
+    const playlist = getPlaylistById(id, type);
 
     if (!playlist) return `<h1>Playlist không tồn tại</h1>`;
 
     const listSongs = playlist.songIds.map(songId => {
         return getSongById(songId);
     }).filter(song => song !== undefined); // Lọc bỏ trường hợp bài hát bị xóa (undefined)
-
-    const nothing = listSongs.length === 0 ? `<div class="empty">Playlist này chưa có bài hát nào</div>` : ``;
-    const header = listSongs.length !== 0 ?
-    `<div class="track-header">
-        <div class="col-index">#</div>
-        <div class="col-title">Tiêu đề</div>
-        <div class="col-artist">Nghệ sĩ</div>
-        <div class="col-album">Album</div>
-        <div class="col-time"><span class="material-icons-round">schedule</span></div>
-    </div>` : ``;
-
+    
     return `
         <div class="playlist-detail">
             
